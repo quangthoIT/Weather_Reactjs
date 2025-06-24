4f0799aecb9d84763d47484d82fd2035c132e5da
quangthoit04 <quangthoit04@gmail.com>
2025-06-25 00:41:19 +0700

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <>
       <NavBar />
+      <Header />
     </>
   );
 };
